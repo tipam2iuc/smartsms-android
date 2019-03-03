7f7541b9602af4c8787f43fdfa58390b4fe7aa25
Kouayip yves <kamwayves37@gmail.com>
2019-03-03 22:41:39 +0100

    Menu

--- app/src/main/java/com/example/lamchard/smartsms/MainActivity.java
+++ app/src/main/java/com/example/lamchard/smartsms/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.lamchard.smartsms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.settings_id:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.apropos_id:
+                Toast.makeText(this, "A propos Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search_id:
+                Toast.makeText(this, "Search Icon Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
